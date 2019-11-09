c48086cf4ffdd6b5ba7acddf5dfef05a984848b0
ishwar silwal <ishwarsilwal12@yahoo.com>
2019-11-09 16:15:53 -0600

    Revert "no message"
    This reverts commit 89595c47be9b5b62fa970705b622b97c19428800.

--- Pods/Target Support Files/AKSideMenu/AKSideMenu-dummy 3.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AKSideMenu : NSObject
-@end
-@implementation PodsDummy_AKSideMenu
-@end
--- Pods/Target Support Files/Pods-RemindMe/Pods-RemindMe-dummy 3.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RemindMe : NSObject
-@end
-@implementation PodsDummy_Pods_RemindMe
-@end
